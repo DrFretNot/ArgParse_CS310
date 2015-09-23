26151533a1d67d47f059266141c6a84812d250f4
Sinh Nguyen <snguyen0219@gmail.com>
2015-09-23 12:11:36 -0500

    Adds package called Library with Library class and LibraryUnitTests

---
+++ Library/src/test/java/edu/jsu/mcis/LibraryUnitTests.java
@@ -0,0 +1,20 @@
+package edu.jsu.mcis;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import Library;
+
+public class LibraryUnitTests {
+    
+    @Test
+    public void testAddArgumentAndReturnDefault() {
+        Library
+    }
+    
+    @Test
+    public void testReverseIt() {
+        HelloWorld h = new HelloWorld();
+        assertEquals("dlrow olleh", h.reverseIt());
+    }
+        
+}
