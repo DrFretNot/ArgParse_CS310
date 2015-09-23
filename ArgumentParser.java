c9d72d362cb94e69c09c39898c40c3c06220f005
rwarren14 <rwarren@stu.jsu.edu>
2015-09-22 20:59:02 -0500

    This is a start for feature # 2 (-h)

---
+++ ArgumentParser.java
@@ -0,0 +1,11 @@
+public void Parser(string{} args){
+
+    for(int i = 0;i < args.length; i++){
+        args[i];
+        
+        
+    }   
+}
+        if s.equals("-h"){
+            getHelpUsageInfo(); // we have to make this method (getHelpUsageInfo) to hold all the -h usages.  
+        }
