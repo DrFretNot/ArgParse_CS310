7f030e3511047ae054c97e6f9d114b34f5af5b30
agreenadvantage <sctrunner@gmail.com>
2015-09-24 16:31:54 -0500

    Now Accepting Args (in the demo)
    added command line argument feature to the VolumeCalculator demo class

--- VolumeCalculator.java
+++ VolumeCalculator.java
@@ -10,14 +10,32 @@ public class VolumeCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       VolumeCalculator v = new VolumeCalculator();
-       System.out.println(v.calculator(3, 4, 5));
+        VolumeCalculator v = new VolumeCalculator();
+        int length, width, height;
+        length = Integer.parseInt(args[0]);
+        width = Integer.parseInt(args[1]);
+        height = Integer.parseInt(args[2]);
+        System.out.println(v.calculator(length, width, height));
+      /*  if (args[0] == "-h")
+       {
+           System.out.println(v.getHelp());
+       }
+       else 
+       {
+            //len wid hi
+       } */
        
     }
     
     public int calculator(int x, int y, int z){
         int volume = (x*y*z);
         return volume;
+    }
+    
+    private String getHelp ()
+    {
+        String help = "usage: java VolumeCalculator length width height\nCalculate the volume of a box.\npositional arguments: \nlength the length of the box\nwidth the width of the box\nheight the height of the box\n";
+        return help;
     }
     
     public VolumeCalculator(){
