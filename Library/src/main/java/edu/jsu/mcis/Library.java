ee823d398878a2f5ad74668934a1f1581d21c9b1
katielkwood <katielkwood@gmail.com>
2015-09-23 22:37:51 -0500

    Wrote testAddArgumentAndReturnDefault()
    I wrote the unit test testAddArgumentAndReturnDefault() but did not
    write any code in the library yet. The test fails.

--- Library/src/main/java/edu/jsu/mcis/Library.java
+++ Library/src/main/java/edu/jsu/mcis/Library.java
@@ -2,11 +2,16 @@ package edu.jsu.mcis;
 
 
 public class Library {
+	
 	public Library(){
-		
+	}
+	public void addArgName(String argName){
+	
 	}
     
-   
+   public String getArgValue(String argument){
+   		return "false";
+   }
     
 }
 
--- Library/src/test/java/edu/jsu/mcis/LibraryUnitTests.java
+++ Library/src/test/java/edu/jsu/mcis/LibraryUnitTests.java
@@ -2,19 +2,21 @@ package edu.jsu.mcis;
 
 import org.junit.*;
 import static org.junit.Assert.*;
-import Library;
 
 public class LibraryUnitTests {
     
     @Test
     public void testAddArgumentAndReturnDefault() {
-        Library
+    	Library l = new Library();
+    	l.addArgName("length");
+        assertEquals("", l.getArgValue("length"));
+        //assert(false);
     }
     
-    @Test
+    /*@Test
     public void testReverseIt() {
         HelloWorld h = new HelloWorld();
         assertEquals("dlrow olleh", h.reverseIt());
-    }
+    }*/
         
 }
