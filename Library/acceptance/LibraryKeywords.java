6aec36369929fb873b2de1bf10dbec9fdf3901ab
Andrew Green <sctrunner@gmail.com>
2015-09-28 22:37:14 -0500

    Added keywords, edited test file, included logs reports and outputs
    The acceptance tests needed a specific format that wasn't there the
    pipes were interfering with the tests.

--- Library/acceptance/LibraryKeywords.java
+++ Library/acceptance/LibraryKeywords.java
@@ -2,15 +2,26 @@ import edu.jsu.mcis.*;
 
 public class LibraryKeywords 
 {
-    private Library lib = new Library();
-    
+    private Library lib;
+    private String output;
+
     public void startVolumeCalculatorWithArguments(String[] args) 
     {
+        lib = new Library();
         lib.addProgramName("Volume Calculator");
         lib.addArgName("length");
         lib.addArgName("width");
         lib.addArgName("height");
         lib.addArgsFromCLI(args);
+        output = lib.checkNumOfArgs(args);
+        if (output != ""){}
+        else
+        {
+            float len = Float.parseFloat(lib.getArgValue("length"));
+            float wid = Float.parseFloat(lib.getArgValue("width"));
+            float hei = Float.parseFloat(lib.getArgValue("height"));
+            output = String.valueOf(len * wid * hei);
+        }
     }
     
     public String getLength() 
@@ -28,12 +39,41 @@ public class LibraryKeywords
         return lib.getArgValue("height");
     }
     
-    public float getProgramOutput() 
+    public String getProgramOutput() 
     {
-        float len = Float.parseFloat(lib.getArgValue("length"));
-        float wid = Float.parseFloat(lib.getArgValue("width"));
-        float hei = Float.parseFloat(lib.getArgValue("height"));
-        return len * wid * hei;
+        //float len = Float.parseFloat(lib.getArgValue("length"));
+        //float wid = Float.parseFloat(lib.getArgValue("width"));
+        //float hei = Float.parseFloat(lib.getArgValue("height"));
+        //String.valueOf(len * wid * hei);
+        return output;
     }
     
+    public void startAbsurdProgramWithArguments(String[] args)
+    {
+        lib = new Library();
+        lib.addProgramName("Absurd Program");
+        lib.addArgName("pet");
+        lib.addArgName("number");
+        lib.addArgName("rainy");
+        lib.addArgName("bathrooms");
+        lib.addArgsFromCLI(args);
+        output = lib.checkNumOfArgs(args);
+    }
+    
+    public String getPet()
+    {
+        return lib.getArgValue("pet");
+    }
+    public String getNumber()
+    {
+        return lib.getArgValue("number");
+    }
+    public String getRainy()
+    {
+        return lib.getArgValue("rainy");
+    }
+    public String getBathrooms()
+    {
+        return lib.getArgValue("bathrooms");
+    }
 }
