74df323779ee56c10d3427da7e83d6860baf0759
rwarren14 <rwarren@stu.jsu.edu>
2015-09-30 21:56:50 -0500

    changes to openpressRob.sh to open html files in web browers when the .sh is run.

--- Library/onepressRob.sh
+++ Library/onepressRob.sh
@@ -1,8 +1,13 @@
 gradle clean build jacocoTestReport
 cd /Users/robertwarren/GitHub/ArgParse_CS310/Library/acceptance
 ./runacceptanceRob.sh
-open index.html
-open /build/reports/jacoco/test/html/index.html
-echo "Running acceptence tests..."
 open report.html
+cd ..
+cd build/reports/jacoco/test/html/
+open index.html
+cd ..
+cd ..
+cd ..
+cd tests/
+open index.html
 
