e60d08b6e2c1718ef136464691b34a0b16bef176
rwarren14 <rwarren@stu.jsu.edu>
2015-09-30 21:07:47 -0500

    hello world

--- Library/onepressRob.sh
+++ Library/onepressRob.sh
@@ -1,3 +1,8 @@
 gradle clean build jacocoTestReport
 cd /Users/robertwarren/GitHub/ArgParse_CS310/Library/acceptance
 ./runacceptanceRob.sh
+open index.html
+open /build/reports/jacoco/test/html/index.html
+echo "Running acceptence tests..."
+open report.html
+
