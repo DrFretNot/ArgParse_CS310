e50b9c72987508921fbf4b6a07641ac6a1f48943
Trent Ford <trentford@Trents-MacBook-Air.local>
2015-10-28 11:28:44 -0500

    Added a shell and batch file for the VolumeCalculator demo
    added VolCal.bat and VolCal.sh for demo running automation

---
+++ demos2/VolCal.sh
@@ -0,0 +1,7 @@
+javac -cp .:../Library/build/libs/Library-1.0.jar VolumeCalculator.java
+java -cp .:../Library/build/libs/Library-1.0.jar VolumeCalculator 3 5 7
+
+java -cp .:../Library/build/libs/Library-1.0.jar VolumeCalculator 5 5 5
+java -cp .:../Library/build/libs/Library-1.0.jar VolumeCalculator 7 something 7
+java -cp .:../Library/build/libs/Library-1.0.jar VolumeCalculator -h
+
