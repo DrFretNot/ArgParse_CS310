320546add495d35a76274b7a8b75ab9ff0ce1d91
rwarren14 <rwarren@stu.jsu.edu>
2015-12-01 17:01:34 -0600

    refoctoring api: changed parameters to describe parameters instead of classes. more to follow.

--- ArgumentParser/src/main/java/edu/jsu/mcis/PositionalArgument.java
+++ ArgumentParser/src/main/java/edu/jsu/mcis/PositionalArgument.java
@@ -1,42 +1,93 @@
 package edu.jsu.mcis;
 
+/**
+*
+*Class positional argument creates a required argument to be read from the command line in a specified order. It includes position, argument name, description, value set, value and argument type. If not specified, the type defaults to a string and description defaults to an empty string.
+*
+*/
 public class PositionalArgument extends Argument{
 
-	//private T value;
-	//private String value;
 	private int position = -1;
-
+    
+    /**
+    *Initializes a positional argument object with only the specified <b>name</b>.
+    *@param name String to be set as <b>name</b>.   
+    *
+    *
+    */
     public PositionalArgument(String name){
         super(name);
     }
     
+    /**
+    *
+    *@param PositionalArgument Initializes a positional argument object with the specified <b>name</b>, <b>dataType</b>, and default desctiption.
+    *
+    *
+    */
     public PositionalArgument(String name, ArgType dataType){
         super(name, dataType);
     }
     
+    /**
+    *
+    *@param PositionalArgument Initializes a positional argument object with the specified <b>name</b>, <b>argDescription</b> and the default type.
+    *
+    *
+    */
     public PositionalArgument(String name, String argDescription){
         super(name, argDescription);
     }
     
+    /**
+    *
+    *@param PositionalArgument Initializes a positional argument object with the specified  <b>name</b>, <b>ArgType</b>, <b>dataType</b>, and <b>argDescription</b>
+    *
+    *
+    */
     public PositionalArgument(String name, ArgType dataType, String argDescription){
         super(name, dataType, argDescription);
     } 
     
+    /**
+    *
+    *@param PositionalArgument Initializes a positional argument object with the specified <b>name</b> and <b>setOfValues</b>.
+    *
+    *
+    */
     public PositionalArgument(String name, String[] setOfValues){
         super(name);
         valueSet = setOfValues;
     }
     
+    /**
+    *
+    *@param PositionalArgument Initializes a positional argument object with the specified <b>name</b>, <b>dataType</b>, <b>setOfValues</b> and default desctiption.
+    *
+    *
+    */
     public PositionalArgument(String name, ArgType dataType, String[] setOfValues){
         super(name, dataType);
         valueSet = setOfValues;
     }
     
+    /**
+    *
+    *@param PositionalArgument Initializes a positional argument object with the specified <b>name</b>, <b>argDescription</b>, <b>setOfValues</b> and the default type.
+    *
+    *
+    */
     public PositionalArgument(String name, String argDescription, String[] setOfValues){
         super(name, argDescription);
         valueSet = setOfValues;
     }
     
+    /**
+    *
+    *@param PositionalArgument Initializes a positional argument object with the specified <b>name</b>, <b>dataType</b> <b>, argDescription</b> and <b>setOfValues</b>.
+    *
+    *
+    */
     public PositionalArgument(String name, ArgType dataType, String argDescription, String[] setOfValues){
         super(name, dataType, argDescription);
         valueSet = setOfValues;
@@ -44,21 +95,45 @@ public class PositionalArgument extends Argument{
 
 
 //***********************************************************************************
-	public void setPosition(int pos){
+	
+    /**
+    *
+    *@param setPosition Sets the position of the positional argument object.
+    *
+    *
+    */
+    public void setPosition(int pos){
 		position = pos;
 	}
 	
+    /**
+    *
+    *@param getPosition Returns the integer position of the positional argument object. Numbering starts at 0.
+    *@returns position
+    *
+    */
 	public int getPosition(){
 		return position;
 	}
 	
 //****** These are subclassed from the abstract class Argument **************************
 
-   
+   /**
+    *
+    *
+    *
+    *
+    */
     public String getName(){
         return this.argName;
     }
     
+    /**
+    *
+    *
+    *
+    *
+    */
     public String getType(){
     	if(type == ArgType.INTEGER){
     		return "integer";
@@ -72,14 +147,32 @@ public class PositionalArgument extends Argument{
     	else return "boolean";
     }
     
+    /**
+    *
+    *
+    *
+    *
+    */
     public String getDescription(){
     	return description;
     }
     
+    /**
+    *
+    *
+    *
+    *
+    */
     public void setValue(String argValue){
     	value = argValue;
     }
 	
+    /**
+    *
+    *
+    *
+    *
+    */
     public <T> T getValue(){
     	if(this.getType().equals("integer")){
     		int intValue = Integer.parseInt(value);
@@ -98,6 +191,12 @@ public class PositionalArgument extends Argument{
     	}
     }
     
+    /**
+    *
+    *
+    *
+    *
+    */
     public String[] getValueSet(){
     	return valueSet;
     }
